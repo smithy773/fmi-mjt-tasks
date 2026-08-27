24ec6e991d2f776a0e17f34d65c66b4a093be497
Viktor Kolev <viktorkolev0707@gmail.com>
2026-08-27 12:27:51 +0300

    Add CosineSimilarity logic

--- homework-JobMatch/src/main/java/matching/CosineSimilarity.java
+++ homework-JobMatch/src/main/java/matching/CosineSimilarity.java
@@ -2,7 +2,7 @@ package matching;
 
 import model.entity.Skill;
 
-import java.util.Set;
+import java.util.*;
 
 public class CosineSimilarity implements SimilarityStrategy {
     /**
@@ -15,6 +15,71 @@ public class CosineSimilarity implements SimilarityStrategy {
      */
     @Override
     public double calculateSimilarity(Set<Skill> candidateSkills, Set<Skill> jobSkills) {
-        return 0;
+        if (candidateSkills == null) {
+            throw new IllegalArgumentException("Can't calculate similarity - candidateSkills are null!");
+        }
+
+        if (jobSkills == null) {
+            throw new IllegalArgumentException("Can't calculate similarity - jobSkills are null!");
+        }
+
+        Map<String, Integer> candidateSkillMap = new HashMap<>();
+        Map<String, Integer> jobSkillMap = new HashMap<>();
+
+        for (Skill candidateSkill : candidateSkills) {
+            candidateSkillMap.put(candidateSkill.name(), candidateSkill.level());
+        }
+
+        for (Skill jobSkill : jobSkills) {
+            jobSkillMap.put(jobSkill.name(), jobSkill.level());
+        }
+
+        HashSet<String> skillUnion = new HashSet<>(jobSkillMap.keySet());
+        skillUnion.addAll(candidateSkillMap.keySet());
+
+        List<String> sortedUnion = skillUnion.stream().sorted(String::compareTo).toList();
+
+        List<Integer> candidateSkillVector = new ArrayList<>();
+        List<Integer> jobSkillVector = new ArrayList<>();
+
+        for (int i = 0; i < sortedUnion.size(); i++) {
+            String currSkill = sortedUnion.get(i);
+            int candidateSkillVal = 0;
+            int jobSkillVal = 0;
+
+            if (candidateSkillMap.containsKey(currSkill)) {
+                candidateSkillVal = candidateSkillMap.get(currSkill);
+            }
+
+            if (jobSkillMap.containsKey(currSkill)) {
+                jobSkillVal = jobSkillMap.get(currSkill);
+            }
+
+            candidateSkillVector.add(candidateSkillVal);
+            jobSkillVector.add(jobSkillVal);
+        }
+
+        int dotProduct = 0;
+        int ApowSum = 0;
+        int BpowSum = 0;
+
+        for (int i = 0; i < sortedUnion.size(); i++) {
+            int candidateLevel = candidateSkillVector.get(i);
+            int jobLevel = jobSkillVector.get(i);
+
+            dotProduct += (candidateLevel * jobLevel);
+
+            ApowSum += (int) Math.pow(candidateLevel, 2);
+            BpowSum += (int) Math.pow(jobLevel, 2);
+        }
+
+        double A = Math.sqrt(ApowSum);
+        double B = Math.sqrt(BpowSum);
+
+        if (A == 0 && B == 0) {
+            return 0;
+        }
+
+        return dotProduct / (A * B);
     }
 }
